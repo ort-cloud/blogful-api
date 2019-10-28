1a51f26bc77bc37b4a5abef132929bf271869955
Carl Ortutay <carlorty@gmail.com>
2019-10-27 20:48:41 -0400

    completed and working

--- src/app.js
+++ src/app.js
@@ -31,6 +31,27 @@ app.get("/articles", (req, res, next) => {
     .catch(next);
 });
 
+app.get("/articles/:article_id", (req, res, next) => {
+  const knexInstance = req.app.get("db");
+  ArticlesService.getById(knexInstance, req.params.article_id)
+    .then(article => {
+      if (!article) {
+        return res.status(404).json({
+          error: {message: `Article doesn't exist`},
+        });
+      }
+
+      res.json({
+        id: article.id,
+        title: article.title,
+        style: article.style,
+        content: article.content,
+        date_published: new Date(article.date_published),
+      });
+    })
+    .catch(next);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
