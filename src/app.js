902a6361fbc2e74ca2d5f0c86ee5a43bebe750a2
Carl Ortutay <carlorty@gmail.com>
2019-11-04 19:17:27 -0500

    updating router, service

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const {NODE_ENV} = require("./config");
-const ArticlesService = require("./articles-service");
+const articlesRouter = require("./articles/articles-router");
 
 const app = express();
 
@@ -14,42 +14,11 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-app.get("/articles", (req, res, next) => {
-  const knexInstance = req.app.get("db");
-  ArticlesService.getAllArticles(knexInstance)
-    .then(articles => {
-      res.json(
-        articles.map(article => ({
-          id: article.id,
-          title: article.title,
-          style: article.style,
-          content: article.content,
-          date_published: new Date(article.date_published),
-        }))
-      );
-    })
-    .catch(next);
-});
+app.use("/api/articles", articlesRouter);
 
-app.get("/articles/:article_id", (req, res, next) => {
-  const knexInstance = req.app.get("db");
-  ArticlesService.getById(knexInstance, req.params.article_id)
-    .then(article => {
-      if (!article) {
-        return res.status(404).json({
-          error: {message: `Article doesn't exist`},
-        });
-      }
-
-      res.json({
-        id: article.id,
-        title: article.title,
-        style: article.style,
-        content: article.content,
-        date_published: new Date(article.date_published),
-      });
-    })
-    .catch(next);
+app.get("/xss", (req, res) => {
+  res.cookie("secretToken", "1234567890");
+  res.sendFile(__dirname + "/xss-example.html");
 });
 
 app.get("/", (req, res) => {
